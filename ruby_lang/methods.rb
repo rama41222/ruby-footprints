f5504f88c1ea6ed5b98a13da20f0c1673f2090d3
rama41222 <dinushankanrg@gmail.com>
2019-08-25 18:19:40 +0530

    refactor: refactored the code

--- ruby_lang/methods.rb
+++ ruby_lang/methods.rb
@@ -25,19 +25,20 @@ num_two = gets.chomp
 puts "Select Op 1-multi 2-add 3-div 4-sub 5-mod"
 opt = gets.chomp
 
-result = case opt
-	when '1'
-		multi num_one, num_two
-	when '2'
-		add num_one, num_two
-	when '3'
-	 	div num_one, num_two
-	when '4'		
-		sub num_one, num_two
-	when '5'
-		mod num_one, num_two
-	else 
-		"Invalid Opteration"
+result =
+case opt
+when '1'
+  multi num_one, num_two
+when '2'
+  add num_one, num_two
+when '3'
+	div num_one, num_two
+when '4'
+  sub num_one, num_two
+when '5'
+  mod num_one, num_two
+else
+  "Invalid Opteration"
 end
 
 puts result
