f925f4745b196531a8e21cb2bce7d7517c8974f3
rama41222 <dinushankanrg@gmail.com>
2019-08-25 14:27:43 +0530

    feat: cli inputs

--- ruby_lang/hello_world.rb
+++ ruby_lang/hello_world.rb
@@ -17,3 +17,19 @@ end
 
 greet "abc"
 
+# Strings
+
+puts "abc" + "def"
+puts "abc #{hello}"
+# Shifted to irb console for strings
+
+# Input from CLI
+puts "No 1: "
+num_one = gets.chomp
+puts "#{num_one} + No 2:"
+num_two = gets.chomp
+puts "#{num_one} + #{num_two} = #{ num_one.to_i + num_two.to_i }"
+
+
+# times
+20.times { puts "20 times"}
