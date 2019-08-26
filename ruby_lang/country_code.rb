ba66e6dbb17ee06072dc6fdda8ac103a34a0516a
rama41222 <dinushankanrg@gmail.com>
2019-08-27 03:37:34 +0530

    feat: country code program

---
+++ ruby_lang/country_code.rb
@@ -0,0 +1,19 @@
+$codes = { lk: 94, ind: 91, ger: 49, us: 1 }
+$ops = { y: 1, n: 0 }
+
+def select_code(country)
+  $codes[country.to_sym] ? $codes[country.to_sym] : 'Invalid Country'
+end
+
+def start(command)
+  $ops[command.to_sym] == 1 ? select_code(command) : false 
+end
+
+loop do
+  puts "Input a country code: "
+  code = gets.chomp
+  puts select_code(code)
+  puts "continue ? "
+  cmd = gets.chomp
+  break if $ops[cmd.to_sym] != 1
+end
