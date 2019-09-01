452a73ebe6fec428499c55406e5472f94f186a74
rama41222 <dinushanka.nayomal@my.sliit.lk>
2019-09-02 02:11:20 +0530

    feat: user model

--- ruby_lang/user_manager/user.rb
+++ ruby_lang/user_manager/user.rb
@@ -1,3 +1,29 @@
-class User
+require 'json'
 
+class User
+  atter_accesssor :name, :email, :permissions
+
+  # *args is an array f arguments
+  def initialize(*args)
+    @name = args[0]
+    @email = args[1]
+    @permissions = User.permissions_from_template
+  end
+
+  def self.permissions_from_template
+    file = File.read('permission.json')
+    JSON.load(file, nil, symbolize_names: true)
+  end
+
+  def self.save
+    user = {
+        :name => @name,
+        :email => @email,
+        :permissions => @permissions.to_json
+    }
+    open('users.json') do |file|
+      file.puts user
+    end
+  end
 end
+
