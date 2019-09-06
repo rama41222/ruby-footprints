f9504d9714dee86f6e4f1105333784d5785ac2a8
rama41222 <dinushanka.nayomal@my.sliit.lk>
2019-09-07 02:49:04 +0530

    added controller methods

--- first_step/app/controllers/welcome_controller.rb
+++ first_step/app/controllers/welcome_controller.rb
@@ -1,9 +1,22 @@
 class WelcomeController < ApplicationController
+
   def home
     puts "Home"
   end
 
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def delete
+
+  end
+
   def about
-    
+
   end
 end
